a885cb8441c9ac5c4e0d855cbf300d05c9c85e5f
m4teuss <mateuus.s.silva@gmail.com>
2026-03-14 00:24:24 -0300

    configurado report hml/json

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+
   e2e: {
     baseUrl: "https://fakestoreapi.com", // URL base projeto
     specPattern: "cypress/e2e/**/*.js",
